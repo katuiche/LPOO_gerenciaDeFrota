6c47823d3eb2cc261592657840654982b94b3b49
Levi Gomes <lv.durana@gmail.com>
2016-11-30 18:33:22 -0200

    Adicionar modelo da tabela e métodos setters pra classe Cliente.

--- src/trabalho/locadora/Cliente.java
+++ src/trabalho/locadora/Cliente.java
@@ -52,6 +52,26 @@ public class Cliente {
     public int getId() {
         return id;
     }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
     
     
     
